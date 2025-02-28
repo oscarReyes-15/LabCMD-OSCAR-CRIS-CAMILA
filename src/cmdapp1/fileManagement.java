a87ca1931fc37047ece272afcd0d7111da0938b2
crisrive-007 <riveracristopher2007@gmail.com>
2025-02-28 14:31:56 -0600

    Funcion escribir

--- src/cmdapp1/fileManagement.java
+++ src/cmdapp1/fileManagement.java
@@ -3,140 +3,149 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cmdapp1;
-    
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 
 /**
- * 
- *     Manejo de Archivos - Dentro de la unidad de memoria secundaria
-
-Tipos de referencias
- 1. Clase utilitaria (file y folder): File
- 2. Clases para archivos de texto:FileWriter y FileReader
- 3. Clases para archivos binarios: RandomAccessFile
-
-////////////////////////////////////////////////////////////////////
-    La clase File
-Sirve para operaciones de utileria sobre un archivo o directorio:
-- crear, borrar, renombrear o ver info acerca del nombre o tamano de algun file o directory
-- *** no lee o escribe 
-
-Formato:
-    " File obj - new File (direccion); "
-
-La direccion del archivo o directorio que se quiere manejar, y puede ser de dos tipos:
- 1. Abstractas o relativas
-    - Le toca a Java intuir su direccion raiz, ej: patito.txt
- 2. Absolutas
-    - Toda la direccion completa: c:\algo\patito.pdf
-
-SOLAMENTE en el constructor se indica la direccion del archivo o directorio que se quiere manejar, no existe ninguna funcion
-para cambiarla...
-
-Funciones basicas:
--------------------------------------------------------------------------------
-|- boolean exists()                  |- Si el archivo existe o no.
-|- boolean isFile() o isDirectory()  |- Si es archivo o directorio
-|- long length()                     |- Especial para archivos: su tamano en bytes
-|- long lastModified()               |- La fecha en milisegundos de su ultima modificacion
-|- boolean isHidden()                |- Si esta escondido o no
-|- boolean canWrite() o canRead()    |- Si pueden escribir o leer
-|- String getName()                  |- El nombre del archivo o directorio
-|- String getAbsolutePath()          |- La dirrecion absoluta
-|- String getParent()                |- La direccion padre. OJO: Solo si esta incluida en el getPath()
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-*/
+ *
+ * Manejo de Archivos - Dentro de la unidad de memoria secundaria
+ *
+ * Tipos de referencias 1. Clase utilitaria (file y folder): File 2. Clases para
+ * archivos de texto:FileWriter y FileReader 3. Clases para archivos binarios:
+ * RandomAccessFile
+ *
+ * //////////////////////////////////////////////////////////////////// La
+ * clase File Sirve para operaciones de utileria sobre un archivo o directorio:
+ * - crear, borrar, renombrear o ver info acerca del nombre o tamano de algun
+ * file o directory - *** no lee o escribe * Formato: " File obj - new File
+ * (direccion); "
+ *
+ * La direccion del archivo o directorio que se quiere manejar, y puede ser de
+ * dos tipos: 1. Abstractas o relativas - Le toca a Java intuir su direccion
+ * raiz, ej: patito.txt 2. Absolutas - Toda la direccion completa:
+ * c:\algo\patito.pdf
+ *
+ * SOLAMENTE en el constructor se indica la direccion del archivo o directorio
+ * que se quiere manejar, no existe ninguna funcion para cambiarla...
+ *
+ * Funciones basicas:
+ * -------------------------------------------------------------------------------
+ * |- boolean exists() |- Si el archivo existe o no. |- boolean isFile() o
+ * isDirectory() |- Si es archivo o directorio |- long length() |- Especial para
+ * archivos: su tamano en bytes |- long lastModified() |- La fecha en
+ * milisegundos de su ultima modificacion |- boolean isHidden() |- Si esta
+ * escondido o no |- boolean canWrite() o canRead() |- Si pueden escribir o leer
+ * |- String getName() |- El nombre del archivo o directorio |- String
+ * getAbsolutePath() |- La dirrecion absoluta |- String getParent() |- La
+ * direccion padre. OJO: Solo si esta incluida en el getPath()
+ * '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+ *
+ */
 public class fileManagement {
+
     private File myFile = null;
-    
-    void setFile(String direccion){
+
+    void setFile(String direccion) {
         myFile = new File(direccion);
     }
-    
-    String info () {
-        
-        if (myFile.exists()){
-            return "\nNombre: " + myFile.getName() + 
-            "\nPath: " + myFile.getPath() + 
-            "\nAbsoluta: " + myFile.getAbsolutePath() + 
-            "\nBytes: " + myFile.length() +
-            "\nModificado en: " + new Date (myFile.lastModified()) +
-            "\nPadre: " + myFile.getAbsoluteFile().getParentFile().getName() +
-            (myFile.isFile()? "\nES FILE\n": (myFile.isDirectory()? "\nES UN FOLDER\n": "\nQUE ES ESTO\n"));
-            
+
+    String info() {
+
+        if (myFile.exists()) {
+            return "\nNombre: " + myFile.getName()
+                    + "\nPath: " + myFile.getPath()
+                    + "\nAbsoluta: " + myFile.getAbsolutePath()
+                    + "\nBytes: " + myFile.length()
+                    + "\nModificado en: " + new Date(myFile.lastModified())
+                    + "\nPadre: " + myFile.getAbsoluteFile().getParentFile().getName()
+                    + (myFile.isFile() ? "\nES FILE\n" : (myFile.isDirectory() ? "\nES UN FOLDER\n" : "\nQUE ES ESTO\n"));
+
         } else {
             return "\nNO EXISTE\n";
         }
     }
-    
-    boolean crearFile () throws IOException {
+
+    boolean crearFile() throws IOException {
         return myFile.createNewFile();
     }
-    
-    boolean crearFolder () {
+
+    boolean crearFolder() {
         return myFile.mkdir();
     }
-    
-    boolean borrar () {
+
+    boolean borrar() {
         return myFile.delete();
     }
-    
-    public boolean borrarCarpeta () {
-        if (myFile.isDirectory()){
+
+    public boolean borrarCarpeta() {
+        if (myFile.isDirectory()) {
             borrarCarpeta(myFile);
             return myFile.delete();
         }
         return false;
     }
-    
-    private void borrarCarpeta (File file) {
-        
+
+    private void borrarCarpeta(File file) {
+
         for (File f : file.listFiles()) {
-            if (f.isDirectory() == true){
-                if (f.listFiles() != null){
+            if (f.isDirectory() == true) {
+                if (f.listFiles() != null) {
                     borrarCarpeta(f);
                 }
                 f.delete();
-            }
-            else if (f.isFile() == true){
+            } else if (f.isFile() == true) {
                 f.delete();
             }
         }
     }
-    
-    
-    String dir () {
-        if(myFile.isDirectory()){
-            
+
+    String dir() {
+        if (myFile.isDirectory()) {
+
             System.out.println("Folder: " + myFile.getName());
             int dirs = 0, files = 0, bytes = 0;
-            
-            for (File child : myFile.listFiles()){
+
+            for (File child : myFile.listFiles()) {
                 System.out.println(new Date(child.lastModified()));
-                if (child.isDirectory()){
+                if (child.isDirectory()) {
                     System.out.print("\t<DIR>\t");
                     dirs++;
                 }
-                if (child.isFile()){
+                if (child.isFile()) {
                     System.out.print("\t<FILE>\t");
                     System.out.println(child.length());
-                    files ++;
+                    files++;
                     bytes += child.length();
                 }
                 System.out.print("\t" + child.getName());
             }
-            
-            System.out.println("\n(" +files + ")" + " files y (" + dirs + ") dirs" );
+
+            System.out.println("\n(" + files + ")" + " files y (" + dirs + ") dirs");
             System.out.println("bytes: " + bytes);
-            
-            } else {
-                System.out.println("Accion no permitida");
-            }
+
+        } else {
+            return "Accion no permitida";
+        }
+        return null;
+    }
+
+    public boolean escribir(String texto) throws IOException {
+        if (texto != null) {
+            FileWriter escritor = new FileWriter(myFile, true);
+            BufferedWriter buffer = new BufferedWriter(escritor);
+
+            buffer.write(texto);
+            buffer.newLine();
+
+            buffer.close();
+            escritor.close();
+            return true;
+        } else {
+            return false;
         }
     }
-    
-    
+}
